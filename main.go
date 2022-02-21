dfdbd8b6739ddec41dead8bf443cfc0b950b6f62
Alain Anghelidi <alainanghelidi@gmail.com>
2022-02-21 09:10:22 +0100

    [FEAT] Fetch link of animes per letters

--- main.go
+++ main.go
@@ -8,11 +8,20 @@ import (
 
 func main() {
 	c := colly.NewCollector(
+		// Restrict domains to myanimelist.net
 		colly.AllowedDomains("myanimelist.net"),
+		// Local cache to prevent multiples download
+		colly.CacheDir("./my_anime_list_cache"),
+		// Limit to depth 0 for debugging
+		colly.MaxDepth(0),
 	)
 
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	c.OnHTML("div[id=horiznav_nav] > ul > li > a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
+		switch e.Text {
+		case "Upcoming", "Just Added":
+			return
+		}
 		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
 		c.Visit(e.Request.AbsoluteURL(link))
 	})
@@ -21,6 +30,6 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	c.Visit("https://myanimelist.net")
+	c.Visit("https://myanimelist.net/anime.php#")
 
 }
